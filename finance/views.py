934e0f7aa5c9885e9ae2d56b17b52d3b18d186b4
sagar.giri <sagar.giri@bebit.com>
2021-10-21 22:56:31 +0900

    Fixes #5

--- finance/views.py
+++ finance/views.py
@@ -1,30 +1,31 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django_filters.views import FilterView
 
-from .models import StockHistory
 from .filters import StockFilter
+from .models import StockHistory
 
 
 class LandingView(TemplateView):
-    template_name = 'index.html'
+	template_name = 'index.html'
 
 
 class StockHistoryListHTMLView(FilterView, ListView):
-    model = StockHistory
-    template_name = "stockhistory_list.html"
-    http_method_names = ('get', 'post',)
-    paginate_by = 10
+	model = StockHistory
+	template_name = "stockhistory_list.html"
+	http_method_names = ('get', 'post',)
+	paginate_by = 100
 
-    def get_context_data(self, **kwargs):
-        context = super(StockHistoryListHTMLView, self).get_context_data(**kwargs)
-        context['filter'] = StockFilter(self.request.GET, queryset=self.get_queryset())
-        return context
+	def get_context_data(self, **kwargs):
+		context = super(StockHistoryListHTMLView, self).get_context_data(**kwargs)
+		context['filter'] = StockFilter(self.request.GET, queryset=self.get_queryset())
+		return context
 
-    def get_queryset(self):
-        return StockHistory.objects.all().values("pk", "company_symbol__symbol", "date", "open", "high", "low", "close",
-                                                 "change_percent")
+	def get_queryset(self):
+		return StockHistory.objects.all()\
+			.order_by("-date")\
+			.values("pk", "company_symbol__symbol", "date", "open", "high", "low", "close", "change_percent")
 
 
 class StockHistoryDetailHTMLView(DetailView):
-    model = StockHistory
-    template_name = "stock_detail.html"
+	model = StockHistory
+	template_name = "stock_detail.html"
