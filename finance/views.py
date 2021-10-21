bb87a0194cc21bbcff9d6f415fe6b995d274ca41
sagar.giri <sagar.giri@bebit.com>
2021-10-21 22:58:01 +0900

    Convert indent

--- finance/views.py
+++ finance/views.py
@@ -6,26 +6,26 @@ from .models import StockHistory
 
 
 class LandingView(TemplateView):
-	template_name = 'index.html'
+    template_name = 'index.html'
 
 
 class StockHistoryListHTMLView(FilterView, ListView):
-	model = StockHistory
-	template_name = "stockhistory_list.html"
-	http_method_names = ('get', 'post',)
-	paginate_by = 100
+    model = StockHistory
+    template_name = "stockhistory_list.html"
+    http_method_names = ('get', 'post',)
+    paginate_by = 100
 
-	def get_context_data(self, **kwargs):
-		context = super(StockHistoryListHTMLView, self).get_context_data(**kwargs)
-		context['filter'] = StockFilter(self.request.GET, queryset=self.get_queryset())
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(StockHistoryListHTMLView, self).get_context_data(**kwargs)
+        context['filter'] = StockFilter(self.request.GET, queryset=self.get_queryset())
+        return context
 
-	def get_queryset(self):
-		return StockHistory.objects.all()\
-			.order_by("-date")\
-			.values("pk", "company_symbol__symbol", "date", "open", "high", "low", "close", "change_percent")
+    def get_queryset(self):
+        return StockHistory.objects.all()\
+            .order_by("-date")\
+            .values("pk", "company_symbol__symbol", "date", "open", "high", "low", "close", "change_percent")
 
 
 class StockHistoryDetailHTMLView(DetailView):
-	model = StockHistory
-	template_name = "stock_detail.html"
+    model = StockHistory
+    template_name = "stock_detail.html"
